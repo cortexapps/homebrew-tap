ddfcddc1db372d376af37bbc037881c19beba87a
Jeff Schnitter <jeff.schnitter@cortex.io>
2025-11-04 11:25:55 -0800

    cortexapps-cli 1.2.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/cortexapps-cli.rb
+++ Formula/cortexapps-cli.rb
@@ -2,8 +2,8 @@ class CortexappsCli < Formula
   include Language::Python::Virtualenv
   desc     "Command-line Interface for Cortexapps"
   homepage "https://github.com/cortexapps/cli"
-  url "https://pypi.io/packages/source/c/cortexapps_cli/cortexapps_cli-1.1.0.tar.gz"
-  sha256  "c980f69670d71d628a6987a01066314e0eb3cb0e3d71ac2261d0534ee348b9cd"
+  url "https://pypi.io/packages/source/c/cortexapps_cli/cortexapps_cli-1.2.0.tar.gz"
+  sha256  "5311be1bff3b5de94113ca2290300490e6f060d084c33d8efb3cb4e5dbea3d01"
   license "MIT"
 
   depends_on "python@3.11"
